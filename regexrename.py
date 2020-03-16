0bb7cf4bc202a2d0b3aa4e56fa60bf32c29235ac
Dominik Jain <djain@gmx.net>
2020-03-16 21:55:34 +0100

    Establish Python 3 compatibility

--- regexrename.py
+++ regexrename.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8
 
+from __future__ import print_function
 import re
 import os
 from sys import argv, exit
@@ -22,29 +23,28 @@ def capFirstChar(s):
 def titleCaps(s):    
     return " - ".join(map(lambda item: capFirstChar(re.sub('[0-9a-zA-ZäöüÄÖÜßéèáà\']+', capitalizeMatch, item)), s.split(" - ")))    
 
-print "\nRegExRename (C) by Dominik Jain\n"
+print("\nRegExRename (C) by Dominik Jain\n")
 
 def exitWithHelp(exitCode=0):
-    print len(argv), argv
-    print " usage: regexrename [options] <mask> [pattern|option] ...\n"
-    print "  mask:     mask with which to filter (may contain the wildcards *,?)"
-    print "  options:  -p,-preview      preview only, no renames take place"
-    print "            -c,-caps         capitalize all words"
-    print "            -t,-title        capitalize all words as in titles"
-    print "            -ix,-ignoreext   ignore file extension"
-    print "            -r,-recurse      recurse subdirectories"
-    print "            -sa,-showall     show all matched files, even if not renamed"
-    print "            -f               process files only, skip directories"
-    print "  patterns: an arbitrary number of regex patterns can be specified"
-    print "            pattern format: \"old:new\""
-    print "               old: regular expression to search for"
-    print "               new: replacement"
-    print "                    $dir is replaced by the name of the dir the file is in"
-    print "            syntax: http://docs.python.org/lib/re-syntax.html"
-    print "            groups can be referenced with \\number, starting with 1, e.g."
-    print "            \"s\\d\\de(\\d\\d):episode \\1\" will replace 's01e02' with 'episode 02'"
-    print "  capitalization takes place after pattern replacement"
-    print '  regexrename can be used to move files by prepending a path, e.g. "\A:../"'
+    print(" usage: regexrename [options] <mask> [pattern|option] ...\n")
+    print("  mask:     mask with which to filter (may contain the wildcards *,?)")
+    print("  options:  -p,-preview      preview only, no renames take place")
+    print("            -c,-caps         capitalize all words")
+    print("            -t,-title        capitalize all words as in titles")
+    print("            -ix,-ignoreext   ignore file extension")
+    print("            -r,-recurse      recurse subdirectories")
+    print("            -sa,-showall     show all matched files, even if not renamed")
+    print("            -f               process files only, skip directories")
+    print("  patterns: an arbitrary number of regex patterns can be specified")
+    print("            pattern format: \"old:new\"")
+    print("               old: regular expression to search for")
+    print("               new: replacement")
+    print("                    $dir is replaced by the name of the dir the file is in")
+    print("            syntax: http://docs.python.org/lib/re-syntax.html")
+    print("            groups can be referenced with \\number, starting with 1, e.g.")
+    print("            \"s\\d\\de(\\d\\d):episode \\1\" will replace 's01e02' with 'episode 02'")
+    print("  capitalization takes place after pattern replacement")
+    print('  regexrename can be used to move files by prepending a path, e.g. "\A:../"')
     exit(exitCode)
 
 argv = argv[1:]
@@ -79,15 +79,15 @@ for i, arg in enumerate(argv):
         elif arg in ("-f", ):
             filesOnly = True
         else:
-            print "ERROR: unknown switch '%s'\n\n", arg
+            print("ERROR: unknown switch '%s'\n\n", arg)
             exitWithHelp(1)
     else:
         if len(patterns) > 0:
-            print "ERROR: invalid argument sequence starting at %s\n\n" % str(argv[i:])
+            print("ERROR: invalid argument sequence starting at %s\n\n" % str(argv[i:]))
             exitWithHelp(1)
         filemask = arg
 if filemask is None:
-    print "ERROR: no filemask specified\n\n"
+    print("ERROR: no filemask specified\n\n")
     exitWithHelp(1)
 
 patterns = [p.split(":") for p in patterns]
@@ -124,9 +124,9 @@ def doReplace(filename, dirpath):
     
     rename = new_filename != filename
     if rename or showAll:
-        print "In %s:\n   %s" % (abspath, filename)
-        if rename: print " > %s\n" % new_filename
-        else: print    
+        print("In %s:\n   %s" % (abspath, filename))
+        if rename: print(" > %s\n" % new_filename)
+        else: print()
     if rename and not preview:
         os.rename(os.path.join(dirpath, filename), os.path.join(dirpath, new_filename))
 
@@ -142,4 +142,4 @@ else:
             doReplace(filename, ".")
  
 if preview:
-    print "PREVIEW only. No files were renamed."
+    print("PREVIEW only. No files were renamed.")
